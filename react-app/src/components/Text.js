80ec9c0086003c6c89fcbf3e43357d0df3831f1d
Soniya Jaiswal <soniyajaiswal995@gmail.com>
2023-07-26 18:09:45 +0530

    new

--- react-app/src/components/Text.js
+++ react-app/src/components/Text.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Text.css'
 
 function Text() {
+  const [api,setApi] = useState([]);
+
+  useEffect(()=>{
+    const fetchdata = async() => {
+        try{
+            const res = await fetch('https://api64.ipify.org?format=json');
+            const data = await res.json();
+            setApi(data);
+
+            console.log(data)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    fetchdata();
+  },[])
+    
   return (
     <div>
-    <h2 style={{textAlign: 'center', marginBottom: '2rem'}}>Some random information</h2>
+    <h2 style={{textAlign: 'center', marginBottom: '2rem'}}>Public API</h2>
 
     <div className="box_container">
-        <div className="box_with_text">
-            <div className="boxes"></div>
-            <span>this is some subtext under an illustration or image</span>
+        <div className='box_with_text'>
+            <div className="boxes">
+            <span>{api.ip}</span>
+            </div>
+            <h2>IP V4</h2>
         </div>
-        <div className="box_with_text">
-            <div className="boxes"></div>
-            <span>this is some subtext under an illustration or image</span>
+        <div className='box_with_text'>
+            <div className="boxes">
+            <span>{api.ip}</span>
+            </div>
+            <h2>IP V6</h2>
         </div>
-        <div className="box_with_text">
-            <div className="boxes"></div>
-            <span>this is some subtext under an illustration or image</span>
-        </div>
-        <div className="box_with_text">
-            <div className="boxes"></div>
-            <span>this is some subtext under an illustration or image</span>
-        </div>
+        
     </div>
 </div>
 
